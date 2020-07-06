5840c6bc860da0314014a2fc5c7c411e84dc6341
muslax <muslax@gmail.com>
2020-07-06 07:00:32 +0700

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.router import router
 from core import config
 from db.mongo import connect, close, get_connection
 
 app = FastAPI(title=config.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_event_handler("startup", connect)
 app.add_event_handler("shutdown", close)
